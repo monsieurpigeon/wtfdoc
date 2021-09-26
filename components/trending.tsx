ca7932d2fdb663bd761b7404e7253b96733a7906
monsieurpigeon <maxpige@gmail.com>
2021-09-27 00:42:00 +0200

    fix styling

--- components/trending.tsx
+++ components/trending.tsx
@@ -1,5 +1,5 @@
 import styles from "./trending.module.css";
-import Link from "next/link";
+import { format } from "date-fns";
 
 export default function Trending() {
   const now = Date.now().toString();
@@ -7,9 +7,13 @@ export default function Trending() {
     <div className={styles.wrapper}>
       <div className={styles.title}>TRENDING</div>
       {[12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1].map((i) => {
+        const compare = parseInt(
+          (parseInt(now.slice(0, -i)) + 1 + "0000000000000").slice(0, 13)
+        );
         return (
-          <a href={`/${now.slice(0, -i)}`} style={{ marginLeft: 150 - 10 *i }} className={styles.line}>
-            {now.slice(0, -i)}
+          <a href={`/${now.slice(0, -i)}`} className={styles.line}>
+            <div className={styles.expiration}>{format(new Date(compare), "MM/dd/yyyy hh:mm:ss:SS")}</div>
+            <div>{now.slice(0, -i)}</div>
           </a>
         );
       })}
